bc9294be6a8e1c9269b4cf26bdd1321dee907e16
derick-asane <derickasane@gmail.com>
2024-06-18 07:42:27 +0100

    creating my project struture with important files

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import NavBar from "./components/navbar/NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <NavBar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./brand.css";
+const Brand = () => {
+  return <div>My brand</div>;
+};
+
+export default Brand;
