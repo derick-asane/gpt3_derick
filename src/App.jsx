75d62d64130c14b63c6a14b68b3985f3c92a112f
derick-asane <derickasane@gmail.com>
2024-06-18 18:34:15 +0100

    working on the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import NavBar from "./components/navbar/NavBar";
-
+import Header from "./containers/header/Header";
+import Brand from "./components/brand/Brand";
+import WhatGPT3 from "./containers/whatGPT3/WhatGPT3";
+import Features from "./containers/features/Features";
+import Possibility from "./containers/posibility/Possibility";
+import CTA from "./components/cta/CTA";
+import Blog from "./containers/blog/Blog";
+import Footer from "./containers/footer/Footer";
+import "./App.css";
 function App() {
   return (
     <div className="App">
